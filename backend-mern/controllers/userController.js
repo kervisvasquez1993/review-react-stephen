7141bb196eaac253d05549e421eb85df2b983c77
redvital <kervisvasquez24@gmail.com>
2023-02-18 22:04:02 -0400

    feat:add validate

--- backend-mern/controllers/userController.js
+++ backend-mern/controllers/userController.js
@@ -1,5 +1,12 @@
 import User from "../models/User.js";
 export const userRegisterController = async (req, res) => {
+    // evitar email duplicado
+    const { email } = req.body;
+    const validateEmail = await User.findOne({ email });
+    if (validateEmail) {
+        const error = new Error(`El usuario con el email ${email}`);
+        return res.status(400).json({ msj: error.message });
+    }
     try {
         const user = new User(req.body);
         const userSave = await user.save();
